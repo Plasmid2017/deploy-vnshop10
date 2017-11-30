c268d82a4006bfbd1b46b3b3fb799266f24631c0
Plasmid2017 <30335659+1183151232@users.noreply.github.com>
2017-11-30 19:26:34 +0800

    hhhh

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const shell = require('shelljs')
 const createHandler = require('github-webhook-handler')
 const handler = createHandler({ path: 'webhook', secret: 'sjns1004200361' })
     // 上面的 secret 保持和 GitHub 后台设置的一致
-const haha = 11
+
 const port = 9988
 const projects = ['vnshop.shudong.wang', 'vnshop10', 'vnshop', 'deploy', 'deploy-vnshop']
 
